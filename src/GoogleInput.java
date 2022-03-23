5bc8805dbdb743edf9682af8c47f6a662aa72676
Palme412 <jakepalmer412@gmail.com>
2022-03-23 13:07:59 -0500

    initial commit

---
+++ src/GoogleInput.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class GoogleInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/Jacob/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        WebElement name = driver.findElement(By.cssSelector("input"));
+
+        name.click();
+        name.sendKeys("Rainbows");
+        name.sendKeys(Keys.RETURN);
+
+        driver.quit();
+    }
+}
